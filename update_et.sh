3379b0bab627b0161f24d6a68232e7457be3de57
stefancaspersz <stefan@caspersz.com>
2016-08-18 17:04:01 +1000

    Create update_et.sh

---
+++ update_et.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt -o /tmp/emerging-Block-IPs.txt
+cp /tmp/emerging-Block-IPs.txt /etc
+chmod 644 /etc/emerging-Block-IPs.txt
