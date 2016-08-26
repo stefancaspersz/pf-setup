e446b8472c63a708f9760609583f4148e44d27d1
Stefan Caspersz <stefan.caspersz@rea017606m.win.int.realestate.com.au>
2016-08-26 10:58:57 +1000

    check file length before and after update

--- update-et.sh
+++ update-et.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+wc -l /etc/emerging-Block-IPs.txt | logger -t pf
 curl http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt -o /tmp/emerging-Block-IPs.txt
 cp /tmp/emerging-Block-IPs.txt /etc
 chmod 644 /etc/emerging-Block-IPs.txt
+wc -l /etc/emerging-Block-IPs.txt | logger -t pf
 rm /tmp/emerging-Block-IPs.txt
 pfctl -f /etc/pf.conf
