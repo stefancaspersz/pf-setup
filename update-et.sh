f10d4e50f8ad1d2c90760048568d1f1f184824f8
Stefan Caspersz <stefan.caspersz@rea017606m.win.int.realestate.com.au>
2016-08-30 17:54:19 +1000

    update pf firewall logging to pf.log from asl.conf instead of systlog.conf

--- update-et.sh
+++ update-et.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wc -l /etc/emerging-Block-IPs.txt | logger -t pf
+wc -l /etc/emerging-Block-IPs.txt | logger -t pf -p 5
 curl http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt -o /tmp/emerging-Block-IPs.txt
 cp /tmp/emerging-Block-IPs.txt /etc
 chmod 644 /etc/emerging-Block-IPs.txt
-wc -l /etc/emerging-Block-IPs.txt | logger -t pf
+wc -l /etc/emerging-Block-IPs.txt | logger -t pf -p 5
 rm /tmp/emerging-Block-IPs.txt
 pfctl -f /etc/pf.conf
