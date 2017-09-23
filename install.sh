c848f868805ec3060f2097299187916d88c5d28c
Daniel Scarnecchia <scarnecchia@gmail.com>
2017-09-22 23:25:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ cp etc/pf.anchors/emerging-threats /etc/pf.anchors/emerging-threats
 cp etc/pf.anchors/compromised-ips /etc/pf.anchors/compromised-ips
 mkdir ~/scripts/pf
 cp opt/pf/update-et.sh ~/scripts/pf/update-et.sh
-chmod 540 scripts/pf/update-et.sh
-sh ./scripts/pf/update-et.sh
+chmod 540 ~/scripts/pf/update-et.sh
+sh ~/scripts/pf/update-et.sh
 pfctl -v -n -f /etc/pf.conf
 pfctl -f -e /etc/pf.conf
 ifconfig pflog0 create
